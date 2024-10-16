5c061bcb3a1692da4d81c973ea3c7bf385fef4c9
HaLBH <144716364+HaLBH176637@users.noreply.github.com>
2024-10-16 22:55:12 +0700

    Interact FE & Logic BE

--- app/src/main/java/com/example/ticketbooking/MainActivity.kt
+++ app/src/main/java/com/example/ticketbooking/MainActivity.kt
@@ -1,20 +1,101 @@
 package com.example.ticketbooking
 
 import android.os.Bundle
+import android.os.Handler
+import android.provider.ContactsContract.Data
+import android.view.View
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
+import com.example.ticketbooking.Adapter.SliderAdapter
+import com.example.ticketbooking.Models.SliderItems
+import com.example.ticketbooking.databinding.ActivityMainBinding
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var database: FirebaseDatabase
+    private val sliderHandle=Handler()
+    private val sliderRunnable = Runnable {
+        binding.viewPager2.currentItem = binding.viewPager2.currentItem + 1
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        FirebaseApp.initializeApp(this)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        database = FirebaseDatabase.getInstance()
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+        initBanner()
+    }
+
+    private fun initBanner() {
+        val myRef: DatabaseReference=database.getReference("Banners")
+        binding.progressBarSlider.visibility = View.VISIBLE
+        myRef.addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val lists = mutableListOf<SliderItems>()
+                for (childSnapshot in snapshot.children){
+                    val list = childSnapshot.getValue(SliderItems::class.java)
+                    if(list!=null){
+                        lists.add(list)
+                    }
+                }
+                binding.progressBarSlider.visibility= View.GONE
+                banners(lists)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+    }
+
+    private fun banners(lists: MutableList<SliderItems>) {
+        binding.viewPager2.adapter=SliderAdapter(lists,binding.viewPager2)
+        binding.viewPager2.clipToPadding=false
+        binding.viewPager2.clipChildren=false
+        binding.viewPager2.offscreenPageLimit=3
+        binding.viewPager2.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+        val compositePageTransformer = CompositePageTransformer().apply {
+            addTransformer(MarginPageTransformer(40))
+            addTransformer(ViewPager2.PageTransformer{ page, position ->
+                val r = 1 - Math.abs(position)
+                page.scaleY = 0.85f + r * 0.15f
+            })
         }
+        binding.viewPager2.setPageTransformer(compositePageTransformer)
+        binding.viewPager2.currentItem = 1
+        binding.viewPager2.registerOnPageChangeCallback(object :ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                sliderHandle.removeCallbacks(sliderRunnable)
+            }
+        })
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sliderHandle.postDelayed(sliderRunnable, 2000)
     }
 }
